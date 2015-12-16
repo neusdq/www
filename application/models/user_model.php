5e842c525b2895d83b7c25b06d41de5b822a1adb
pbchen <294306275@qq.com>
2015-12-16 09:46:08 +0800

    pbchen user list

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -41,6 +41,12 @@ class User_model extends CI_Model {
         return $user;
     }
     
+    
+    public function add_user($data){
+        $this->db->insert($this->_user_tb, $data);
+        return $this->db->insert_id();
+    }
+    
     /**
      * 修改用户信息
      * @param type $user_id
@@ -51,6 +57,65 @@ class User_model extends CI_Model {
         $this->db->where('id', $user_id);
         $this->db->update($this->_user_tb, $update_info);
         return $this->db->affected_rows();
+    }
+    
+    /**
+     * 分页条件
+     */
+    public function user_list_page_where(){
+        $cwhere = array();
+        if (isset($_REQUEST['id']) && $_REQUEST['id']!='') {
+            $cwhere['`user`.`id` LIKE '] = '%'.$_REQUEST['id'].'%';
+        }
+        if (isset($_REQUEST['account']) && $_REQUEST['account'] != '') {
+            $cwhere['`user`.`user_name` LIKE '] = '%'.$_REQUEST['account'].'%';
+        }
+        if (isset($_REQUEST['name']) && $_REQUEST['name'] != '') {
+            $cwhere['`user`.`nick_name` LIKE '] = '%'.$_REQUEST['name'].'%';
+        }
+        if (isset($_REQUEST['email']) && $_REQUEST['email']!='') {
+            $cwhere['`user`.`email` LIKE '] = '%'. $_REQUEST['email'] .'%';
+        }
+        return $cwhere;
+    }
+    
+    public function user_list_page_data($dtparser){
+        $cols = array('`id`','`user_name` as `account`','`nick_name`'
+            ,'`email`','`phone`','`role`');
+        $sort_cols = array('0'=>'`id`');
+        $filter_cols = array();
+        //查询主表
+        $dtparser->select($cols, $sort_cols, $filter_cols, FALSE);
+        $dtparser->from($this->_user_tb);
+        //条件
+        $cwhere = $this->user_list_page_where();
+        $d['code'] = 0;
+        $d['iTotal'] = 0;
+        $d['iFilteredTotal'] = 0;
+        $d['aaData'] = array();
+        if( $d['code'] == 0 ){
+            $d['iTotal'] = $dtparser->count($cwhere);
+            $d['iFilteredTotal'] = $d['iTotal'];
+            $query = $dtparser->get($cwhere);
+            $arr = $query->result_array();
+            $this->ajax_list_table_data($arr);
+            $d['aaData']=$arr;
+        }
+        return $d;
+    }
+    
+    private function ajax_list_table_data(&$pageData){
+        $role = $this->role_model->get_role();
+        foreach($pageData as &$v){
+            $role_name = array();
+            $v['oper'] = "<a rel='{$v['id']}' role='{$v['role']}' class='edit oper'>编辑</a>";
+            foreach(explode(',', $v['role']) as $r){
+                if(isset($role[$r])){
+                    $role_name[] = $role[$r];
+                }
+            }
+            $v['role_name'] = implode(',', $role_name);
+        }
     }
     
 }
